6b6e15fe40575f105c82df4431e1a2aa1a2ce901
Sani <isani@talentsoft.com>
2017-03-19 01:11:22 +0100

    add images, action for tournament, and new page

---
+++ src/actions/actionTourn.ts
@@ -0,0 +1,104 @@
+/* eslint consistent-return: 0, no-else-return: 0*/
+import { polyfill } from 'es6-promise';
+import request from 'axios';
+import md5 from 'spark-md5';
+import * as types from '../actions/actionsTypes';
+
+polyfill();
+
+export function makeTournamentRequest(method, id, data, api = '/tournament'){
+    return request[method](api + (id ? ('/' + id) : ''), data);
+}
+
+export function destroy(id){
+    return { type: types.DESTROY_TOURNAMENT, id}
+}
+
+export function typing(text){
+    return {
+        type: types.TYPING,
+        newTournament: text
+    };
+}
+
+export function createTournamentRequest(data){
+    return{
+        type: types.CREATE_TOURNAMENT_REQUEST,
+        id: data.id,
+        players: data.players,
+        description: data.description
+    };
+}
+
+export function createTournamentSuccess(){
+    return{
+        type: types.CREATE_TOURNAMENT_SUCCESS
+    };
+}
+
+export function createTournamentFailure(data){
+    return {
+        type: types.CREATE_TOURNAMENT_FAILURE,
+        id: data.id,
+        error: data.error
+    };
+}
+
+export function createTournamentDuplicate(){
+    return {
+        type: types.CREATE_TOURNAMENT_DUPLICATE
+    };
+}
+
+/**
+ * 
+ * @param data     id: String,
+    description: String,
+    count: {type: Number, min: 0},
+    players: {type: Number, min: 0},
+    date: {type: Date, default: Date.UTC}
+ */
+
+export function createTournament(data){
+    return (dispatch, getState) =>{
+        if(data.description.trim().length <= 0) return;
+        
+        const id = md5.hash(data.description);
+
+        const {tournament} = getState();
+        const finalData = {
+            count: 1,
+            id,
+            players: data.players,
+            description: data.description
+        }
+
+        if(tournament.tournaments.filter(tournamentItem => tournamentItem.id === id).length > 0){
+            return dispatch(createTournamentDuplicate());
+        }
+
+        dispatch(createTournamentRequest(finalData));
+
+        return makeTournamentRequest('post', id, finalData)
+            .then(res => {
+                if(res.status === 200){
+                    return dispatch(createTournamentSuccess());
+                }
+            })
+            .catch(() => {
+                return dispatch(createTournamentFailure({
+                    id,
+                    error: 'Something went wrong and we couldn\'t create your tournament'
+                }))
+            });
+    };
+}
+
+export function destroyTournament(id){
+    return dispatch => {
+        return makeTournamentRequest('delete', id,{})
+            .then(() => dispatch(destroy(id)))
+            .catch(() => dispatch(createTournamentFailure({id,
+                error: 'Something went wrong and we couldn\'t add your tournament'})));
+    };
+}
---
+++ src/pages/TournamentPages.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class TournamentsPage extends React.Component<any,any>{
+    render(){
+        return(
+            <div>
+                <h1>Tournament</h1>
+            </div>
+        );
+    }
+}
+
+export default TournamentsPage;
